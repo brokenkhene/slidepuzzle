0c5da5f6f65dff949cd932089ac420c0fa143bad
DESKTOP-HG0EBF8\broke <brokenekhene@gmail.com>
2018-03-17 19:33:29 +0700

    Video 15

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	void Start ()
+    {
+        float screenRatio = (float)Screen.height / (float)Screen.width;
+        float _100pxFitCameraSize = screenRatio / 2;
+        float _164pxFitCameraSize = _100pxFitCameraSize * 164 / 100;
+        Camera.main.orthographicSize = _164pxFitCameraSize;
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField]
+    RectTransform buttonStart;
+
+    [SerializeField]
+    RectTransform buttonReset;
+
+    void Start()
+    {
+        float expandRate = Screen.width / 164f;
+
+        //buton start
+        buttonStart.sizeDelta = new Vector2(buttonStart.sizeDelta.x * expandRate, buttonStart.sizeDelta.y * expandRate);
+
+        float buttonStartY = Screen.height / 2 - Screen.width / 2 - (Screen.height / 2 - Screen.width / 2) / 2 - buttonStart.sizeDelta.y / 2;
+        buttonStart.position = new Vector3(Screen.width /2 , buttonStartY, buttonStart.position.z);
+
+        //button reset
+        buttonReset.sizeDelta = new Vector2(buttonReset.sizeDelta.x * expandRate, buttonReset.sizeDelta.y * expandRate);
+
+        float buttonResetY = Screen.height / 2 - Screen.width / 2 - (Screen.height / 2 - Screen.width / 2) / 2 - buttonReset.sizeDelta.y / 2;
+        buttonReset.position = new Vector3(Screen.width / 2, buttonResetY, buttonReset.position.z);
+
+    }
+
+    void Update () {
+		
+	}
+}
